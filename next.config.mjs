ba8533ecd1b5239e604a746aaf6e5254c8d67a59
Abdulm <abdulmfmuti@gmail.com>
2026-03-02 13:42:42 +0700

    recovery next

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+        pathname: '/**',
+      },
+    ],
   },
-}
+  reactStrictMode: true,
+};
 
-export default nextConfig
+export default nextConfig;
